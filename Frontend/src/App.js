b92a44062e2a5d662560105647c74edbb80bea77
Ira Levkovich <6346390@mail.ru>
2020-12-25 23:46:57 +0300

    react-bootstrap setup, header & footer component, cdnjs(font-awesome)

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const App=()=> {
+const App = () => {
   return (
     <>
-    <h1>Welcome to I-Shop</h1>
+      <Header />
+        <Container>
+      <main className='py-3'>
+        <h1>Welcome to I-Shop</h1>
+      </main>
+      </Container>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
