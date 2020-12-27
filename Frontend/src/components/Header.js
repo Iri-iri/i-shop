79ec59185984fb0b05c84870ed7a2f23c669f45b
Ira Levkovich <6346390@mail.ru>
2020-12-27 21:00:19 +0300

    add products list and folder of images

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -10,8 +10,10 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
+              <Nav.Link href='/phone'><i className="fas fa-phone"></i> Phone</Nav.Link>
+              <Nav.Link href='/favorites'><i className="fas fa-heart"></i> Favorites</Nav.Link>
               <Nav.Link href='/cart'><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-              <Nav.Link href='/login'><i className="far fa-user"></i> Sign in</Nav.Link>
+              <Nav.Link href='/login'><i className="fas fa-user"></i> Sign in</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
