08755c35843223464f7a623c8348e9837634dee2
Ira Levkovich <6346390@mail.ru>
2021-01-11 01:59:56 +0300

    fetching products from react

--- Frontend/src/pages/ProductPage.js
+++ Frontend/src/pages/ProductPage.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductPage = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProducts] = useState({})
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <Link className='btn btn-dark my-3' to='/'>
